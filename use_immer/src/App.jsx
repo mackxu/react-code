db19ae8d8ee95ca5e891629d41586b64c57c69c4
duoduoxu <duoduoxu@tencent.com>
2024-09-30 17:50:07 +0800

    feat: use-immer demo

---
+++ use_immer/src/App.jsx
@@ -0,0 +1,56 @@
+import { useImmer } from 'use-immer';
+
+const initialProducts = [{
+  id: 0,
+  name: 'Baklava',
+  count: 1,
+}, {
+  id: 1,
+  name: 'Cheese',
+  count: 5,
+}, {
+  id: 2,
+  name: 'Spaghetti',
+  count: 2,
+}];
+
+export default function App() {
+  const [
+    products,
+    setProducts
+  ] = useImmer(initialProducts)
+
+  function handleIncreaseClick(productId) {
+    setProducts((draft) => {
+      const product = draft.find(p => p.id === productId);
+      product.count += 1;
+    })
+  }
+
+  const handleDecreaseClick = (productId) => {
+    setProducts((draft) => {
+      const product = draft.find(p => p.id === productId);
+      product.count -= 1;
+    })
+  }
+
+  return (
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>
+          {product.name}
+          {' '}
+          (<b>{product.count}</b>)
+          <button onClick={() => {
+            handleIncreaseClick(product.id);
+          }}>
+            +
+          </button>
+          <button onClick={() => handleDecreaseClick(product.id)}>
+            –
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
